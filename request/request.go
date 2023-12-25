b1256348351946afa851c785c00f46e77022d381
stl3 <32008952+stl3@users.noreply.github.com>
2023-12-25 13:48:55 +0800

    Changed user agent

--- request/request.go
+++ request/request.go
@@ -2,47 +2,45 @@ package request
 
 import (
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"time"
 )
 
-const agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8"
+const defaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 
 // Request is a base function for sending HTTP requests.
 func Request(client *http.Client, method string, url string, header http.Header) (*http.Client, *http.Response, http.Header, error) {
 	if client == nil {
-		// Make a new http client with cookie jar if no existing client is provided
+		// Make a new http client with a cookie jar if no existing client is provided
 		jar, _ := cookiejar.New(nil)
 		client = &http.Client{Timeout: 30 * time.Second, Jar: jar}
 	}
+
 	// Build a new request
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		// logrus.Errorln(err)
 		return nil, nil, nil, err
 	}
 
 	// Set headers
 	if header != nil {
 		if _, ok := header["User-Agent"]; !ok {
-			header.Set("User-Agent", agent)
+			header.Set("User-Agent", defaultUserAgent)
 		}
 		req.Header = header
 	}
 
 	// Do request
-	// logrus.Debugf("Sending %v request to %v with headers %v\n", req.Method, req.URL, req.Header)
 	res, err := client.Do(req)
 	if err != nil {
-		// logrus.Errorln(err)
 		return nil, nil, nil, err
 	}
 	return client, res, req.Header, nil
 }
 
-// Get wraps the Request function, sends a HTTP GET request, returns the smae client and the html of the content body.
+// Get wraps the Request function, sends an HTTP GET request, and returns the same client and the HTML of the content body.
 func Get(client *http.Client, url string, headers map[string]string) (*http.Client, string, error) {
 	header := http.Header{}
 	for k, v := range headers {
@@ -53,9 +51,11 @@ func Get(client *http.Client, url string, headers map[string]string) (*http.Clie
 		return nil, "", err
 	}
 	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		return nil, "", errors.New(res.Status)
+
+	if res.StatusCode != http.StatusOK {
+		return nil, "", errors.New("request failed with status: " + res.Status)
 	}
-	content, err := ioutil.ReadAll(res.Body)
+
+	content, err := io.ReadAll(res.Body)
 	return client, string(content), err
 }
