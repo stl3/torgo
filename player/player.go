47ae1d7be0967bd5ea2a38844f75463676b0f6b3
stl3 <32008952+stl3@users.noreply.github.com>
2023-12-25 13:06:47 +0800

    :crying_cat_face:
    Added support for setting window titles to the title of the movie playing:
    mpv `--force-media-title=`
    vlc `--meta-title=`
    Changed some `log.Printf` stuff
    Added stuff to help debugging

--- player/player.go
+++ player/player.go
@@ -5,29 +5,41 @@ We did some modifications on it in order to let it fit into 'torrodle'
 package player
 
 import (
+	"log"
 	"os/exec"
 	"runtime"
 	"strings"
-
-	"github.com/sirupsen/logrus"
 )
 
 // Players holds structs of all supported players.
 var Players = []Player{
 	{
-		Name:            "mpv",
-		DarwinCommand:   []string{"mpv"},
-		LinuxCommand:    []string{"mpv"},
-		WindowsCommand:  []string{"mpv"},
+		Name:           "mpv",
+		DarwinCommand:  []string{"mpv"},
+		LinuxCommand:   []string{"mpv"},
+		WindowsCommand: []string{"mpv", "--no-terminal"}, // Default
+		// WindowsCommand:  []string{"mpv", "--profile=movie-flask", "--no-terminal"}, // Just for use with my mpv profile
 		SubtitleCommand: "--sub-file=",
+		TitleCommand:    "--force-media-title=", // Shows the movie folder name as title instead of http://localhost:port
 	},
 	{
-		Name:            "vlc",
-		DarwinCommand:   []string{"/Applications/VLC.app/Contents/MacOS/VLC"},
-		LinuxCommand:    []string{"vlc"},
-		WindowsCommand:  []string{"%ProgramFiles%\\VideoLAN\\VLC\\vlc.exe"},
+		Name:          "vlc",
+		DarwinCommand: []string{"/Applications/VLC.app/Contents/MacOS/VLC"},
+		LinuxCommand:  []string{"vlc"},
+		// WindowsCommand:  []string{"%ProgramFiles%\\VideoLAN\\VLC\\vlc.exe"},
+		WindowsCommand:  []string{"vlc.exe"}, // vlc player should be in users env path in case installed in non-default path
 		SubtitleCommand: "--sub-file=",
+		TitleCommand:    "--meta-title=", //
 	},
+	// {
+	// 	Name: "kmp",
+	// 	// DarwinCommand: []string{"/Applications/VLC.app/Contents/MacOS/VLC"},
+	// 	// LinuxCommand:  []string{"vlc"},
+	// 	// WindowsCommand:  []string{"%ProgramFiles%\\VideoLAN\\VLC\\vlc.exe"},
+	// 	WindowsCommand:  []string{"Kmplayer.exe"},
+	// 	SubtitleCommand: "--sub-file=",
+	// 	// TitleCommand:    "--title=",
+	// },
 }
 
 // Player manages the execiution of a media player.
@@ -37,12 +49,14 @@ type Player struct {
 	LinuxCommand    []string
 	WindowsCommand  []string
 	SubtitleCommand string
+	TitleCommand    string
 	started         bool
 }
 
 // Start launches the Player with the given command and arguments in subprocess.
-func (player *Player) Start(url string, subtitlePath string) {
-	if player.started == true {
+func (player *Player) Start(url string, subtitlePath string, title string) {
+	// if player.started == true {
+	if player.started {
 		// prevent multiple calls
 		return
 	}
@@ -59,16 +73,41 @@ func (player *Player) Start(url string, subtitlePath string) {
 	if subtitlePath != "" {
 		command = append(command, player.SubtitleCommand+subtitlePath)
 	}
-	logrus.Debugf("command: %v\n", command)
+	if title != "" {
+		command = append(command, player.TitleCommand+title)
+	}
+	log.Printf("\x1b[36mLaunching player:\x1b[0m \x1b[33m%v\x1b[0m\n", command)
+	// logrus.Debugf("command: %v\n", command)
 	cmd := exec.Command(command[0], command[1:]...)
+	// cmd.Stdout = os.Stdout // show player output to console
+	// cmd.Stdout = os.NewFile(0, os.DevNull)
+	// cmd.Stderr = os.Stderr
+
 	player.started = true
-	go cmd.Start()
+
+	if err := cmd.Start(); err != nil {
+		log.Printf("Error starting player: %v\n", err)
+		return
+	}
+	// Wait for the player process to complete
+	if err := cmd.Wait(); err != nil {
+		exitErr, ok := err.(*exec.ExitError)
+		if ok {
+			log.Printf("Player exited with non-zero status: %v\n", exitErr.ExitCode())
+		} else {
+			log.Printf("Error waiting for player: %v\n", err)
+		}
+	}
+
+	// Reset the started flag to allow for subsequent calls
+	player.started = false
+
 }
 
 // GetPlayer returns the Player struct of the given player name.
 func GetPlayer(name string) *Player {
 	for _, player := range Players {
-		if strings.ToLower(player.Name) == strings.ToLower(name) {
+		if strings.EqualFold(player.Name, name) {
 			return &player
 		}
 	}
