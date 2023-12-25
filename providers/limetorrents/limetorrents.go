61f3a32e36e86928b4ca591b60ea648f526f1dab
stl3 <32008952+stl3@users.noreply.github.com>
2023-12-25 13:32:14 +0800

    Add files via upload

--- providers/limetorrents/limetorrents.go
+++ providers/limetorrents/limetorrents.go
@@ -2,28 +2,60 @@ package limetorrents
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/dustin/go-humanize"
 	"github.com/sirupsen/logrus"
 
-	"github.com/tnychn/torrodle/models"
-	"github.com/tnychn/torrodle/request"
+	"github.com/stl3/torrodle/models"
+	"github.com/stl3/torrodle/request"
 )
 
 const (
 	Name = "LimeTorrents"
-	Site = "https://www.limetorrents.info"
+	// Site = "https://www.limetorrents.info"
+	// Site = "https://www.limetorrents.lol"
+	// DefaultSite is the default LimeTorrents site URL
+	DefaultSite     = "https://www.limetorrents.lol"
+	AlternativeSite = "https://www.limetorrents.info"
 )
+
+var Site string // Package-level variable
 
 type provider struct {
 	models.Provider
 }
 
+func checkSiteAvailability(siteURL string) bool {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	_, err := client.Get(siteURL)
+	return err == nil
+}
+
 func New() models.ProviderInterface {
+	var Site string
+
+	if checkSiteAvailability(DefaultSite) {
+		Site = DefaultSite
+		// log.Printf("Using site: %s\n", Site)
+	} else if checkSiteAvailability(AlternativeSite) {
+		Site = AlternativeSite
+		// log.Printf("Using site: %s\n", Site)
+	} else {
+		// Both sites are down, you can handle this case accordingly
+		// panic("Both LimeTorrents sites are down")
+		log.Fatal("Both LimeTorrents sites are down")
+	}
+
 	provider := &provider{}
 	provider.Name = Name
 	provider.Site = Site
@@ -42,6 +74,7 @@ func (provider *provider) Search(query string, count int, categoryURL models.Cat
 }
 
 func extractor(surl string, page int, results *[]models.Source, wg *sync.WaitGroup) {
+
 	logrus.Infof("LimeTorrents: [%d] Extracting results...\n", page)
 	_, html, err := request.Get(nil, surl, nil)
 	if err != nil {
