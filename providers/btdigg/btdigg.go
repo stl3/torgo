8504860eb35100aa01736cfae5e24a41e78bcf8b
stl3 <32008952+stl3@users.noreply.github.com>
2023-12-25 22:58:26 +0800

    Add files via upload
    Added provider btdigg

---
+++ providers/btdigg/btdigg.go
@@ -0,0 +1,91 @@
+package btdigg
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/dustin/go-humanize"
+	"github.com/sirupsen/logrus"
+
+	"github.com/stl3/torrodle/models"
+	"github.com/stl3/torrodle/request"
+)
+
+const (
+	Name = "BTDigg"
+	Site = "https://btdig.com"
+)
+
+// var Site string // Package-level variable
+
+type provider struct {
+	models.Provider
+}
+
+func New() models.ProviderInterface {
+	// var Site string
+
+	provider := &provider{}
+	provider.Name = Name
+	provider.Site = Site
+	provider.Categories = models.Categories{
+		All: "/search?q=%v&order=2&p=%d",
+		// All: "/search?q=%v",
+		// All: "/search?q=%v&category=all&orderby=seeders&p=%d",
+	}
+	return provider
+}
+
+func (provider *provider) Search(query string, count int, categoryURL models.CategoryURL) ([]models.Source, error) {
+	results, err := provider.Query(query, categoryURL, count, 50, 1, extractor)
+	return results, err
+}
+
+func extractor(surl string, page int, results *[]models.Source, wg *sync.WaitGroup) {
+
+	logrus.Infof("BTDigg: [%d] Extracting results...\n", page)
+	_, html, err := request.Get(nil, surl, nil)
+	if err != nil {
+		logrus.Errorln(fmt.Sprintf("BTDigg: [%d]", page), err)
+		wg.Done()
+		return
+	}
+
+	var sources []models.Source
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(html))
+	// Log the HTML content
+	// logrus.Debugf("BTDigg: [%d] HTML Content:\n%s", page, html)
+
+	// Find the container of each search result item
+	resultsContainer := doc.Find("div.one_result")
+	logrus.Debugf("BTDigg: [%d] Number of result containers found: %d", page, resultsContainer.Length())
+	// Iterate over each search result item
+	resultsContainer.Each(func(_ int, result *goquery.Selection) {
+		// Extract information from each search result item
+		title := result.Find("div.torrent_name a").Text()
+		URL, _ := result.Find("div.torrent_name a").Attr("href")
+		filesizeStr := result.Find("span.torrent_size").Text()
+		filesize, _ := humanize.ParseBytes(strings.TrimSpace(filesizeStr))
+		magnet, _ := result.Find("div.torrent_magnet a").Attr("href")
+
+		source := models.Source{
+			From:     "BTDigg",
+			Title:    title,
+			URL:      Site + URL,
+			Seeders:  0, // this site gives no seeder/leecher info
+			Leechers: 0,
+			FileSize: int64(filesize),
+			Magnet:   magnet,
+		}
+		sources = append(sources, source)
+
+		// logrus.Debugf("BTDigg: [%d] Result %d - Title: %s, URL: %s, FileSize: %d bytes, Magnet: %s",
+		// 	page, index+1, source.Title, source.URL, source.FileSize, source.Magnet)
+	})
+
+	logrus.Debugf("BTDigg: [%d] Amount of results: %d", page, len(sources))
+	*results = append(*results, sources...)
+	wg.Done()
+}
