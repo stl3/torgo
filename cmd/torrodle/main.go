611cae64d3b30b847d40150a5fbbfd68e45f89a1
stl3 <32008952+stl3@users.noreply.github.com>
2023-12-28 12:12:58 +0800

    Add files via upload

--- cmd/torrodle/main.go
+++ cmd/torrodle/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"os/user"
 	"path/filepath"
@@ -586,6 +588,23 @@ boop~
 		var p *player.Player
 		if playerChoice == "None" {
 			p = nil
+			///////////////
+			cmd := exec.Command("am", "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d", source.URL, "-n", "is.xyz.mpv/.MPVActivity")
+			log.Printf("\x1b[36mLaunching player:\x1b[0m \x1b[33m%v\x1b[0m\n", cmd.Args)
+			if err := cmd.Start(); err != nil {
+				log.Printf("Error starting player: %v\n", err)
+				return
+			}
+			// Wait for the player process to complete
+			if err := cmd.Wait(); err != nil {
+				exitErr, ok := err.(*exec.ExitError)
+				if ok {
+					log.Printf("Player exited with non-zero status: %v\n", exitErr.ExitCode())
+				} else {
+					log.Printf("Error waiting for player: %v\n", err)
+				}
+			}
+			////////////////
 		} else {
 			p = player.GetPlayer(playerChoice)
 		}
