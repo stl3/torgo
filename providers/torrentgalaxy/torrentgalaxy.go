ca0fb45c595fdab5f7d830e627ac6e12f539543f
stl3 <32008952+stl3@users.noreply.github.com>
2024-01-16 08:29:29 +0800

    Add files via upload

--- providers/torrentgalaxy/torrentgalaxy.go
+++ providers/torrentgalaxy/torrentgalaxy.go
@@ -31,35 +31,27 @@ func New() models.ProviderInterface {
 	provider.Name = Name
 	provider.Site = Site
 	provider.Categories = models.Categories{
-		// Changed Jan 2024
-		// Format now takes the first letter from query, and changes space to "-"
-		// Eg https://www.TorrentGalaxy.com/t/the-walking-dead-s11e01/1/
-		// https://www.TorrentGalaxy.com/first_letter_of_query/%v/%d/
-		All:   "torrents.php?search=%v&page=%d",
-		Movie: "torrents.php?c42=1&search=%v&page=%d",
-		TV:    "torrents.php?c41=1&search=%v&page=%d",
-		Porn:  "torrents.php?c35=1&search=%v&page=%d",
+		All:           "/torrents.php?search=%v&sort=id&order=desc&page=%d#results",
+		Movie:         "/torrents.php?c42=1&search=%v&sort=id&order=desc&page=%d#results",
+		TV:            "/torrents.php?c41=1&search=%v&sort=id&order=desc&page=%d#results",
+		Anime:         "/torrents.php?c28=1&search=%v&sort=id&order=desc&page=%d#results",
+		Documentaries: "/torrents.php?genres[]=5&search=%v&sort=id&order=desc&page=%d#results",
+		Porn:          "/torrents.php?c35=1&search=%v&sort=id&order=desc&page=%d#results",
 	}
 	return provider
 }
 
 func (provider *provider) Search(query string, count int, categoryURL models.CategoryURL) ([]models.Source, error) {
 	modifiedQuery := modifyQuery(query)
-	logrus.Infof("Modified query before changes: %s", modifiedQuery)
-	modifiedQuery = string(query[0]) + "/" + modifiedQuery
-	logrus.Infof("Modified query afer changes: %s", modifiedQuery)
+	// logrus.Infof("Query: %s", modifiedQuery)
 	results, err := provider.Query(modifiedQuery, categoryURL, count, 50, 1, extractor)
 	return results, err
 }
 
 func extractor(surl string, page int, results *[]models.Source, wg *sync.WaitGroup) {
-	// Replace "%2F" with "/"
-	surl = strings.ReplaceAll(surl, "%2F", "/")
 	// Log or display the full URL before making the request
 	logrus.Infof("TorrentGalaxy: [%d] Requesting URL: %s\n", page, surl)
-
 	logrus.Infof("TorrentGalaxy: [%d] Extracting results...\n", page)
-	// _, html, err := request.Get(nil, strings.ReplaceAll(surl, "/", "%2F"), nil)
 	_, html, err := request.Get(nil, surl, nil)
 	if err != nil {
 		logrus.Errorln(fmt.Sprintf("TorrentGalaxy: [%d]", page), err)
@@ -69,11 +61,10 @@ func extractor(surl string, page int, results *[]models.Source, wg *sync.WaitGro
 
 	var sources []models.Source
 	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(html))
-	resultsContainer := doc.Find("#content > div.fill-table > table > tbody > tr")
+	resultsContainer := doc.Find("div.tgxtablerow.txlight")
 	resultsContainer.Each(func(_ int, result *goquery.Selection) {
-		// Extract information from each search result item
-		title := result.Find("td.n a").Text()
-
+		title := result.Find("div:nth-child(4) > div > a.txlight > span > b").Text()
+		// logrus.Infof("Title: %s", title)
 		if containsHTMLEncodedEntities(title) {
 			decodedTitle, err := decodeHTMLText(title)
 			if err != nil {
@@ -86,19 +77,16 @@ func extractor(surl string, page int, results *[]models.Source, wg *sync.WaitGro
 			logrus.Infof("Title: %s", title)
 		}
 
-		URL, _ := result.Find("td.n a").Attr("href")
-		sizeStr := result.Find("td:nth-child(6)").Text()
+		URL, _ := result.Find("div:nth-child(2) > div > a.txlight").Attr("href")
+		sizeStr := result.Find("div:nth-child(8) > span").Text()
 		size, err := humanize.ParseBytes(sizeStr)
-		seeders, _ := strconv.Atoi(result.Find("td.s").Text())
-		leechers, _ := strconv.Atoi(result.Find("td.l").Text())
-		magnet, _ := result.Find("td.m a").Attr("href")
-
 		if err != nil {
-			// log.Println("Error converting sizeStr to int:", err)
-			// log.Println("Size string:", sizeStr)
-			// logrus.Errorln("Error converting sizeStr to int:", err)
 			logrus.Infof("Size string: %s", sizeStr)
 		}
+
+		seeders, _ := strconv.Atoi(result.Find("div:nth-child(11) > span > font:nth-child(1) > b").Text())
+		leechers, _ := strconv.Atoi(result.Find("div:nth-child(11) > span > font:nth-child(2) > b").Text())
+		magnet, _ := result.Find("div:nth-child(5) > a:nth-child(2)").Attr("href")
 
 		source := models.Source{
 			From:     "TorrentGalaxy",
@@ -144,13 +132,6 @@ func decodeHTMLText(text string) (string, error) {
 }
 
 func modifyQuery(query string) string {
-	// Take the first letter of the query
-	// firstLetter := string(query[0])
-
-	// Replace spaces with "-"
-	modifiedQuery := strings.ReplaceAll(query, " ", "-")
-	// finalQuery := firstLetter + "/" + modifiedQuery
-	// Construct the modified query
-
+	modifiedQuery := strings.ReplaceAll(query, " ", "+")
 	return modifiedQuery
 }
