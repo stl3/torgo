83a48a6fb31d81754e4b6c70db6a9f547036213d
stl3 <32008952+stl3@users.noreply.github.com>
2024-01-17 01:33:09 +0800

    Add files via upload

--- player/player.go
+++ player/player.go
@@ -8,9 +8,83 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 	"runtime"
 	"strings"
+
+	// "github.com/sirupsen/logrus"
+	"github.com/stl3/torrodle/config"
 )
+
+var u, _ = user.Current()
+var home = u.HomeDir
+var configFile = filepath.Join(home, ".torgo.json")
+
+// var configurations config.TorrodleConfig
+
+// func init() {
+// 	// Load the configuration during package initialization
+// 	loadConfig()
+// }
+
+// // Function to load the configuration
+// func loadConfig() {
+// 	configurations, err := config.LoadConfig(configFile)
+// 	if err != nil {
+// 		fmt.Println("Error loading config:", err)
+// 		configurations = config.TorrodleConfig{}
+// 	}
+// 	fmt.Printf("Loaded configuration: %+v\n", configurations)
+// }
+
+// Declare Mpv_params as a package-level variable
+var MpvParams string
+
+// var configurations config.TorrodleConfig
+
+// var configurations = config.TorrodleConfig{}
+
+// func init() {
+// 	// Load the configuration
+// 	u, _ := user.Current()
+// 	home := u.HomeDir
+// 	configFile := filepath.Join(home, ".torgo.json")
+
+// 	// configurations, err := config.LoadConfig(configFile)
+// 	// if err != nil {
+// 	// 	fmt.Println("Error loading config:", err)
+// 	// 	configurations = config.TorrodleConfig{}
+// 	// }
+// 	// fmt.Printf("Loaded configuration: %+v\n", configurations)
+// }
+
+// func init() {
+// 	// Load the configuration during package initialization
+// 	configurations, err := config.LoadConfig(configFile)
+// 	if err != nil {
+// 		fmt.Println("Error loading config:", err)
+// 		configurations = config.TorrodleConfig{}
+// 	}
+// 	fmt.Printf("Loaded configuration: %+v\n", configurations)
+// }
+
+// func init() {
+// 	// Load the configuration during package initialization
+// 	loadConfig()
+// }
+
+// // Function to load the configuration
+func loadConfig() {
+	configurations, err := config.LoadConfig(configFile)
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		configurations = config.TorrodleConfig{}
+	}
+	// Shows used config options from json
+	// fmt.Printf("Loaded configuration: %+v\n", configurations)
+	MpvParams = configurations.Mpv_params
+}
 
 // Players holds structs of all supported players.
 var Players = []Player{
@@ -19,11 +93,32 @@ var Players = []Player{
 		DarwinCommand: []string{"mpv"},
 		LinuxCommand:  []string{"mpv"},
 		// AndroidCommand: []string{},
-		AndroidCommand: []string{"am", "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d"},
-		// WindowsCommand: []string{"mpv", "--no-resume-playback", "--no-terminal"}, // Default
-		WindowsCommand:  []string{"mpv", "--profile=movie-flask", "--no-resume-playback", "--no-terminal"}, // Just for use with my mpv profile
+		AndroidCommand:  []string{"am", "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d"},
 		SubtitleCommand: "--sub-file=",
 		TitleCommand:    "--force-media-title=", // Shows the movie folder name as title instead of http://localhost:port
+		// Check if configurations.Mpv_params is not empty, and adjust WindowsCommand accordingly
+		WindowsCommand: func() []string {
+			// Load the configuration
+			// // configurations, err := config.LoadConfig(configFile)
+			// // if err != nil {
+			// // 	fmt.Println("Error loading config:", err)
+			// // 	configurations = config.TorrodleConfig{}
+			// // }
+			loadConfig()
+			// // fmt.Println("Mpv_params:", configurations.Mpv_params)
+			fmt.Println("Mpv_params:", MpvParams)
+			// fmt.Printf(configurations.Mpv_params)
+			// logrus.Infof("Mpv_params: %s", configurations.Mpv_params)
+
+			// if configurations.Mpv_params != "" {
+			// 	return []string{"mpv", configurations.Mpv_params, "--no-resume-playback", "--no-terminal"}
+			// }
+			if MpvParams != "" {
+				return []string{"mpv", MpvParams, "--no-resume-playback", "--no-terminal"}
+			}
+			fmt.Println("Mpv_params is empty")
+			return []string{"mpv", "--no-resume-playback", "--no-terminal"}
+		}(),
 	},
 	{
 		Name:          "vlc",
@@ -67,6 +162,20 @@ func (player *Player) Start(url string, subtitlePath string, title string) {
 		return
 	}
 
+	// configurations, err := config.LoadConfig(configFile)
+	// if err != nil {
+	// 	fmt.Println("Error loading config:", err)
+	// 	configurations = config.TorrodleConfig{}
+	// }
+	// fmt.Printf("Loaded configuration: %+v\n", configurations)
+
+	// configurations, err := config.LoadConfig(configFile)
+	// if err != nil {
+	// 	fmt.Println("Error loading config:", err)
+	// 	configurations = config.TorrodleConfig{}
+	// }
+	// fmt.Printf(configurations.Mpv_params)
+
 	var command []string
 	switch runtime.GOOS {
 	case "darwin":
@@ -77,34 +186,24 @@ func (player *Player) Start(url string, subtitlePath string, title string) {
 		command = player.WindowsCommand
 	case "android":
 		command = player.AndroidCommand
-		// // player.startAndroidMPV(url)
-		// mpv_android(url)
-		// player.started = true
 	}
 
 	// Append the video URL to the command for non-Android cases
 	command = append(command, url)
 
 	if player.Name == "mpv" && runtime.GOOS == "android" {
-		// Do something based on the condition
 		fmt.Println("Using mpv")
-		// time.Sleep(3 * time.Second)
 		command = append(command, "-n", "is.xyz.mpv/.MPVActivity")
-		// } else if player.Type == vlc {
 
 	} else if player.Name == "vlc" && runtime.GOOS == "android" {
-		// Do something else
 		fmt.Println("Using VLC")
-		// time.Sleep(3 * time.Second)
 		command = append(command, "-n", "org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity")
 	}
 
 	if subtitlePath != "" && runtime.GOOS != "android" {
-		// if subtitlePath != "" {
 		command = append(command, player.SubtitleCommand+subtitlePath)
 	}
 	if title != "" && runtime.GOOS != "android" {
-		// if title != "" {
 		command = append(command, player.TitleCommand+title)
 	}
 
@@ -112,13 +211,6 @@ func (player *Player) Start(url string, subtitlePath string, title string) {
 	// logrus.Debugf("command: %v\n", command)
 
 	cmd := exec.Command(command[0], command[1:]...)
-	// var cmd *exec.Cmd // Declare cmd outside if-else block
-	// if runtime.GOOS == "android" {
-	// 	cmd = exec.Command(command[0])
-	// } else {
-	// 	cmd = exec.Command(command[0], command[1:]...)
-	// }
-	// time.Sleep(3 * time.Second)
 	player.started = true
 
 	if err := cmd.Start(); err != nil {
@@ -149,43 +241,3 @@ func GetPlayer(name string) *Player {
 	}
 	return nil
 }
-
-// func mpv_android(url string) {
-// 	cmd := exec.Command("am", "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d", url, "-n", "is.xyz.mpv/.MPVActivity")
-// 	err := cmd.Run()
-// 	if err != nil {
-// 		fmt.Println("Error:", err)
-// 	}
-// }
-
-// // startAndroidMPV launches mpv on Android using the specific intent.
-// func (player *Player) startAndroidMPV(url string) {
-
-// 	// time.Sleep(7 * time.Second)
-// 	// cmd0 := exec.Command("mpv-go", url)
-// 	// // cmd0 := exec.Command(player.AndroidCommand[0], "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d", url, "-n", "is.xyz.mpv/.MPVActivity")
-// 	// // err := cmd0.Run()
-// 	// // if err != nil {
-// 	// // 	log.Fatal(err)
-// 	// // }
-// 	cmd := exec.Command(player.AndroidCommand[0], "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d", url, "-n", "is.xyz.mpv/.MPVActivity")
-// 	// // urlWithQuotes := "\"" + url + "\""
-
-// 	// // cmd := exec.Command(player.AndroidCommand[0], "start", "--user", "0", "-a", "android.intent.action.VIEW", "-d", urlWithQuotes, "-n", "is.xyz.mpv/.MPVActivity")
-// 	player.started = true
-// 	log.Printf("\x1b[36mLaunching player:\x1b[0m \x1b[33m%v\x1b[0m\n", cmd.Args)
-// 	if err := cmd.Start(); err != nil {
-// 		log.Printf("Error starting player: %v\n", err)
-// 		return
-// 	}
-// 	// Wait for the player process to complete
-// 	if err := cmd.Wait(); err != nil {
-// 		exitErr, ok := err.(*exec.ExitError)
-// 		if ok {
-// 			log.Printf("Player exited with non-zero status: %v\n", exitErr.ExitCode())
-// 		} else {
-// 			log.Printf("Error waiting for player: %v\n", err)
-// 		}
-// 	}
-// 	player.started = false
-// }
