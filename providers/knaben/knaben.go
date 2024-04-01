7f19a5e523da6253a7086325806d1a66848a2499
stl3 <32008952+stl3@users.noreply.github.com>
2024-04-01 09:31:24 +0800

    Update knaben.go

--- providers/knaben/knaben.go
+++ providers/knaben/knaben.go
@@ -11,8 +11,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/html"
 
-	"github.com/stl3/torrodle/models"
-	"github.com/stl3/torrodle/request"
+	"github.com/stl3/torgo/models"
+	"github.com/stl3/torgo/request"
 )
 
 const (
